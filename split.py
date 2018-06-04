e743650bddc44ffae5835c7772cad88d04b1c1f8
Peter Mackay <mackay.pete+git@gmail.com>
2018-06-04 13:21:36 +0100

    WIP

--- split.py
+++ split.py
@@ -29,6 +29,26 @@ def split_code_file(dst_dir, dst_names, src_path, size):
                         dst_odd.write(odd)
 
 
+def split_gfx_file(dst_dir, dst_names, src_path, size):
+    with open(src_path, "rb") as src:
+        print(src_path)
+        for (dst_name_1, dst_name_2, dst_name_3, dst_name_4) in dst_names:
+            print("\t" + dst_name_1 + ", " + dst_name_2 + ", " + dst_name_3 + ", " + dst_name_4)
+            dst_path_1 = os.path.join(dst_dir, dst_name_1)
+            dst_path_2 = os.path.join(dst_dir, dst_name_2)
+            dst_path_3 = os.path.join(dst_dir, dst_name_3)
+            dst_path_4 = os.path.join(dst_dir, dst_name_4)
+            with open(dst_path_1, "wb") as dst_1:
+                with open(dst_path_2, "wb") as dst_2:
+                    with open(dst_path_3, "wb") as dst_3:
+                        with open(dst_path_4, "wb") as dst_4:
+                            for i in range(size / 2):
+                                dst_1.write(src.read(2))
+                                dst_2.write(src.read(2))
+                                dst_3.write(src.read(2))
+                                dst_4.write(src.read(2))
+
+
 def split_file(dst_dir, dst_names, src_path, size):
     with open(src_path, "rb") as src:
         print(src_path)
@@ -40,13 +60,16 @@ def split_file(dst_dir, dst_names, src_path, size):
                 dst.write(contents)
 
 
-def split_game(dst_game_name, src_game_name, code_files, split_files):
+def split_game(dst_game_name, src_game_name, code_files, gfx_files, split_files):
     dst_dir = os.path.join(root_dir, dst_game_name)
     if not os.path.exists(dst_dir):
         os.mkdir(dst_dir)
     for (dst_names, src_ext, size) in code_files:
         src_path = os.path.join(root_dir, src_game_name, src_game_name + "." + src_ext)
         split_code_file(dst_dir, dst_names, src_path, size)
+    for (dst_names, src_ext, size) in gfx_files:
+        src_path = os.path.join(root_dir, src_game_name, src_game_name + "." + src_ext)
+        split_gfx_file(dst_dir, dst_names, src_path, size)
     for (dst_names, src_ext, size) in split_files:
         src_path = os.path.join(root_dir, src_game_name, src_game_name + "." + src_ext)
         split_file(dst_dir, dst_names, src_path, size)
@@ -63,9 +86,19 @@ sf2_code = [(
     128 * 1024
 )]
 
+sf2_gfx = [(
+    [
+        ("sf2-5m.4a", "sf2-7m.6a", "sf2-1m.3a", "sf2-3m.5a"),
+        ("sf2-6m.4c", "sf2-8m.6c", "sf2-2m.3c", "sf2-4m.5c"),
+        ("sf2-13m.4d", "sf2-15m.6d", "sf2-9m.3d", "sf2-11m.5d"),
+    ],
+    "vrom",
+    512 * 1024
+)]
+
 sf2_files = [
     (["sf2_9.12a"], "z80", 64 * 1024),
     (["sf2_18.11c", "sf2_19.12c"], "oki", 128 * 1024),
 ]
 
-split_game("sf2ub", "StreetFighterII", sf2_code, sf2_files)
+split_game("sf2ub", "StreetFighterII", sf2_code, sf2_gfx, sf2_files)
